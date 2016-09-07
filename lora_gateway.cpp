62b1aa594d7c07355f02904ab9ca531a80af4691
nht198 <nguyenhuytrung198@gmail.com>
2016-09-07 08:16:33 +0700

    Change 10

--- lora_gateway.cpp
+++ lora_gateway.cpp
@@ -46,16 +46,16 @@ int main (int argc, char *argv[]){
 
         while(1)
         {
-                e= sx1272.receivePacketTimeout(1000);
+                e= sx1272.receivePacketTimeoutACK(10000);
                 if (e==0)
                 {
                         printf("packet received success\n");
-//						 printf("%x\n",sx1272.packet_received.length);
-//        for (unsigned int i = 0; i < sx1272.packet_received.length; i++) {
-//                my_packet[i] = (char)sx1272.packet_received.data[i];
-//				printf("%x\n",sx1272.packet_received.data[i]);
-//                        }
-//                printf(my_packet);
+						 printf("%x\n",sx1272.packet_received.length);
+        for (unsigned int i = 0; i < sx1272.packet_received.length; i++) {
+                my_packet[i] = (char)sx1272.packet_received.data[i];
+				printf("%x\n",sx1272.packet_received.data[i]);
+                        }
+                printf(my_packet);
                 }
                 else printf("error\n");
                 delay(4000);
